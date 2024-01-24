fa831822f60b4c075b6fe8c0a2f4a906159c6058
Miguel Flores <154698261+MikeFlores19@users.noreply.github.com>
2024-01-24 15:01:43 -0600

    Add files via upload

---
+++ QUICKSORT & BINARY SEARCH/funciones.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+int cont(int [],int);
+void swapp (int *,int *);
+int partitionn (int [],int,int);
+void quickSort (int [],int,int);
+int binSearch(int [],int,int,int);
+
+#endif // FUNCIONES_H_INCLUDED
